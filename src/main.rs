4115ea7dc375e5d61453d6347e48145167cd7b0f
Ubuntu <azureuser@vm-playground-01.jsrpcwjflawunjbf1pfpssbo2c.cx.internal.cloudapp.net>
2024-08-26 00:27:51 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -22,16 +22,22 @@ fn main() {
                 String::new()
             });
 
-            // Uncomment this block to pass the first stage
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-            }
+            tokenize(&file_contents);
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
             return;
         }
     }
+}
+
+fn tokenize(input: &str) {
+    for char in input.chars() {
+        match char {
+            '(' => println!("LEFT_PAREN ( null"),
+            ')' => println!("RIGHT_PAREN ) null"),
+            _ => {} // Ignore other characters for now
+        }
+    }
+    println!("EOF  null");
 }
