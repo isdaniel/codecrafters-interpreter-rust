b620d73788af900a509411dce83e7b5a5cc24672
Ubuntu <azureuser@vm-playground-01.jsrpcwjflawunjbf1pfpssbo2c.cx.internal.cloudapp.net>
2024-08-28 14:14:37 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,8 @@ fn tokenize(input: &str) {
         match char {
             '(' => println!("LEFT_PAREN ( null"),
             ')' => println!("RIGHT_PAREN ) null"),
+            '{' => println!("LEFT_BRACE {{ null"),
+            '}' => println!("RIGHT_BRACE }} null"),
             _ => {} // Ignore other characters for now
         }
     }
